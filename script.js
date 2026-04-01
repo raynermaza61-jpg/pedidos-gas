16b76aa1121d7b37a5f2f333124a54bbf05143de
raynermaza61-jpg <raynermaza61@gmail.com>
2026-04-01 10:40:19 -0500

    upadate js
    Logo botones

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Intro → mostrar menú
+setTimeout(() => {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("main").classList.remove("hidden");
+}, 3500);
 
+// Mostrar secciones
+function mostrar(seccion) {
+  let secciones = document.querySelectorAll(".section");
+  secciones.forEach(s => s.classList.add("hidden"));
+
+  document.getElementById(seccion).classList.remove("hidden");
+}
